4eedbf5082294746d72dcf30cfd14691f012d4f2
Joseph Arcila <39535338+JosephArcila@users.noreply.github.com>
2023-04-05 17:07:06 +0900

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        title: 'WeaknessWork',
+        home: Scaffold(
+          backgroundColor: Colors.redAccent,
+          appBar: AppBar(
+            title: Text('WeaknessWork'),
+            backgroundColor: Colors.redAccent,
+          ),
+          body: TabataPage(),
+        ),
       ),
     );
+
+class Tabata extends StatefulWidget {
+  @override
+  _TabataState createState() => _TabataState();
+}
+
+class _TabataState extends State<Tabata> {
+  int tabataNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+          onPressed: () {
+            setState(() {
+              tabataNumber = Random().nextInt(8) + 1;
+            });
+          },
+          child: Image.asset('images/tabata$tabataNumber.png')),
+    );
+  }
+}
+
+class TabataPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Tabata();
+  }
+}

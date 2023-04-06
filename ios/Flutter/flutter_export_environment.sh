8519c6f98ed580295edf4c927a9293a2d025cbd2
Joseph Arcila <39535338+JosephArcila@users.noreply.github.com>
2023-04-06 09:02:16 +0900

    Configured for web support

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\josep\AndroidStudioProjects\magic-8-ball-flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\josep\OneDrive\Desktop\WeaknessWork"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"

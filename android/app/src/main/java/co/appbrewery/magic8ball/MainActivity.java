af38a4102b773e8134e162ca389078f12962f226
Joseph Arcila <39535338+JosephArcila@users.noreply.github.com>
2023-05-09 12:34:24 +0900

    Restrict project to web platform only

--- android/app/src/main/java/co/appbrewery/magic8ball/MainActivity.java
+++
@@ -1,8 +0,0 @@
-package co.appbrewery.magic8ball;
-
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-
-public class MainActivity extends FlutterActivity {
-}
-
--- android/app/src/main/kotlin/co/appbrewery/weakness_work/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package co.appbrewery.weakness_work
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
